f9110126a800cad40ea3995bc7d80a6502620cee
Normalstrafe <62997249+Normalstrafe@users.noreply.github.com>
2023-06-24 23:21:10 +0300

    Update app.py

--- web/app.py
+++ web/app.py
@@ -4,9 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    with open('message.txt', 'r') as file:
-        message = file.read()
-    return message
+    return "Hello"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
